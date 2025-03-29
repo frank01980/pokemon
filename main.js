485dd8897cdcc3f3a455b2ee1666ad1ab57ba6d5
frank01980 <mohssinli7wak@gmail.com>
2025-03-29 05:08:11 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>403 Zabroniony</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+        <meta http-equiv="cache-control" content="max-age=0" />
+        <meta http-equiv="cache-control" content="no-cache" />
+        <meta http-equiv="expires" content="0" />
+        <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+        <meta http-equiv="pragma" content="no-cache" />
+        <meta name=â€ťrobotsâ€ť content="noindex">
+        <link rel="shortcut icon" href="/error_pages/img/favicons/favicon.ico"/>
+        <link rel="apple-touch-icon" href="/error_pages/img/favicons/AppIcon60x60.png"/><!-- 60x60 -->
+        <link rel="apple-touch-icon" sizes="76x76" href="https://d3.atthost.pl/error_pages/img/favicons/AppIcon76x76.png"/><!-- 76x76 -->
+        <link rel="apple-touch-icon" sizes="512x512" href="https://d3.atthost.pl/error_pages/img/favicons/iTunesArtwork.png"/><!-- 512x512 -->
+        <link rel="apple-touch-icon" sizes="1024x1024" href="https://d3.atthost.pl/error_pages/img/favicons/iTunesArtwork@2x.png"/><!-- 1024x1024 -->
+        <link href='https://fonts.googleapis.com/css?family=Roboto:100,300&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" ></script>
+        <link rel="stylesheet" href="https://d3.atthost.pl/error_pages/css/style.css">
+        <script>
+            jQuery(document).ready(function($) {
+                $('.block').css({'height': (($(window).height() * 1)) + 'px'});
+                $(window).resize(function() { // On resize
+                    $('.block').css({'height': (($(window).height() * 1)) + 'px'});
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <div class="block">
+            <div class="centered">
+                <p>Serwer zrozumiaĹ‚ zapytanie lecz konfiguracja bezpieczeĹ„stwa zabrania mu zwrĂłciÄ‡ ĹĽÄ…dany zasĂłb.</p>
+                <h1>403 Zabroniony.</h1>
+            </div>
+        </div>
+        <img src="/error_pages/img/logo-atthost-white.png" class="logo" title="AttHost : Serwery, Domeny, Aplikacje" alt="Logo AttHost"/>
+    </body>
+</html>
